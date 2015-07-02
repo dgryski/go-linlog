84555c839fe196c4f88b71e453423a16f3c97d6d
Damian Gryski <damian@gryski.com>
2015-07-02 14:07:24 +0200

    optimize lb and add amd64 asm version

--- linlog.go
+++ linlog.go
@@ -25,38 +25,3 @@ func BinDownOf(size uint64, linear, subbin uint64) (uint64, uint64) {
 
 	return (subIndex << shift), (xrange << subbin) + subIndex
 }
-
-func lb(x uint64) uint64 { return 64 - nlz(x) - 1 }
-
-func nlz(x uint64) uint64 {
-	var n uint64
-
-	n = 1
-
-	if (x >> 32) == 0 {
-		n = n + 32
-		x = x << 32
-	}
-	if (x >> (32 + 16)) == 0 {
-		n = n + 16
-		x = x << 16
-	}
-
-	if (x >> (32 + 16 + 8)) == 0 {
-		n = n + 8
-		x = x << 8
-	}
-
-	if (x >> (32 + 16 + 8 + 4)) == 0 {
-		n = n + 4
-		x = x << 4
-	}
-
-	if (x >> (32 + 16 + 8 + 4 + 2)) == 0 {
-		n = n + 2
-		x = x << 2
-	}
-
-	n = n - (x >> 63)
-	return uint64(n)
-}
