22b45916ec944c987eeb548a296eefdaec53e909
Damian Gryski <dgryski@gmail.com>
2016-05-02 19:16:50 +0200

    asmfmt++

--- lb_amd64.s
+++ lb_amd64.s
@@ -1,11 +1,12 @@
 // +build amd64,!appengine
 
 // func lb(x uint64) uint64
-TEXT ·lb(SB),4,$0-16
-        BSRQ  x+0(FP), AX
-        JZ zero
-        MOVQ AX, ret+8(FP)
-        RET
+TEXT ·lb(SB), 4, $0-16
+	BSRQ x+0(FP), AX
+	JZ   zero
+	MOVQ AX, ret+8(FP)
+	RET
+
 zero:
-        MOVQ $64, ret+8(FP)
-        RET
+	MOVQ $64, ret+8(FP)
+	RET
