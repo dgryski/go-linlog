e754d092cc7414568b19ec58c3237770773c765d
Damian Gryski <dgryski@gmail.com>
2016-05-02 19:37:23 +0200

    docs++

--- linlog.go
+++ linlog.go
@@ -6,18 +6,20 @@ http://pvk.ca/Blog/2015/06/27/linear-log-bucketing-fast-versatile-simple/
 */
 package linlog
 
-func BinOf(size uint64, linear, subbin uint64) (uint64, uint64) {
+// BinOf rounds size as appropriate, and returns the rounded size and bucket number.
+func BinOf(size uint64, linear, subbin uint64) (rounded uint64, bucket uint64) {
 	nBits := lb(size | (1 << uint(linear)))
 	shift := nBits - subbin
 	mask := uint64(1<<shift - 1)
-	rounded := size + mask /* XXX: overflow. */
-	subIndex := rounded >> shift
+	round := size + mask /* XXX: overflow. */
+	subIndex := round >> shift
 	xrange := nBits - linear
 
-	return (rounded & ^mask), (xrange << subbin) + subIndex
+	return (round & ^mask), (xrange << subbin) + subIndex
 }
 
-func BinDownOf(size uint64, linear, subbin uint64) (uint64, uint64) {
+// BinDownOf rounds size down, and returns the rounded size and bucket number.
+func BinDownOf(size uint64, linear, subbin uint64) (rounded uint64, bucket uint64) {
 	nBits := lb(size | (1 << linear))
 	shift := nBits - subbin
 	subIndex := size >> shift
